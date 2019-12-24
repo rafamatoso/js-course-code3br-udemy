d7f6ce906e8101f05e9c2b33e497e0a547f6c1f6
rafamatoso <rafaelmatosog@gmail.com>
2019-12-23 21:46:31 -0300

    Project Created

---
+++ s03-fundamentos/02-comentario.js
@@ -0,0 +1,14 @@
+// Comentário de uma linha
+console.log("Linha 1");
+
+/*
+ Comentário de 
+ múltiplas linhas
+*/
+console.log("Linha 2");
+
+/**
+ * Comentários de
+ * múltiplas linhas (diferente)
+ */
+console.log("Linha 3");
