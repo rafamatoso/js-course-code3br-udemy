49deeeb1a03eb188ddfdd1eb44fb8904fdeb71e8
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-22 20:48:24 -0300

    Promises #5 Parte 2

--- s09-esnext/promises_05.js
+++ s09-esnext/promises_05.js
@@ -1,13 +1,26 @@
+// Encadeamento de chamadas (chaining) com tratamento de erros
+
 function funcionarOuNao(valor, chanceErro) {
   return new Promise((resolve, reject) => {
-    if (Math.random() < chanceErro) {
-      reject("Ocorreu um erro!");
-    } else {
-      resolve(valor);
+    try {
+      con.log("ulala");
+      if (Math.random() < chanceErro) {
+        reject("Ocorreu um erro!");
+      } else {
+        resolve(valor);
+      }
+    } catch (e) {
+      reject(e);
     }
   });
 }
 
-funcionarOuNao("Testando...", 0.9)
-  .then((valor) => console.log(`Valor: ${valor}`))
-  .catch((err) => console.log(`Erro: ${err}`));
+funcionarOuNao("Testando...", 0.5)
+  .then((valor) => `Valor: ${valor}`)
+  .then(
+    (v) => consol.log(v),
+    (err) => console.log(`Erro Específico: ${err}`)
+  )
+  .then(() => console.log("Quase Fim!"))
+  .catch((err) => console.log(`Erro Geral: ${err}`))
+  .then(() => console.log("Fim!"));
