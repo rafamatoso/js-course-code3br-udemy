9b29e46113f2708374cfbf6c342eecc8e7bb2a5a
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-30 20:39:21 -0300

    XMLHttpRequest #2

--- s15-ajax/server.js
+++ s15-ajax/server.js
@@ -9,5 +9,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Transforma o objeto parseado em json
 app.use(bodyParser.json());
 
-app.get("/teste", (req, res) => res.send("Ok"));
+// Vai interpretar o formulário que vem do arquivo de upload
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./upload");
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage }).single("arquivo");
+
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.end("Ocorreu um erro no upload de arquivo.");
+    }
+    res.end("Upload realizado com sucesso.");
+  });
+});
+
 app.listen(8080, () => console.log("Executando..."));
