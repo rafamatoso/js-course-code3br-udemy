f6524bdec0aaa23966699e678838c70668397b99
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-30 12:39:24 -0300

    Flappy Bird #5

--- s14-integrando-html-js-css/js/flappy.js
+++ s14-integrando-html-js-css/js/flappy.js
@@ -112,3 +112,38 @@ function Passaro(alturaJogo) {
 //   barreiras.animar();
 //   passaro.animar();
 // }, 20);
+
+function Progresso() {
+  this.elemento = novoElemento("span", "progresso");
+  this.atualizarPontos = (pontos) => {
+    this.elemento.innerHTML = pontos;
+  };
+  this.atualizarPontos(0);
+}
+
+function FlappyBird() {
+  let pontos = 0;
+
+  const areaDoJogo = document.querySelector("[wm-flappy]");
+  const altura = areaDoJogo.clientHeight;
+  const largura = areaDoJogo.clientWidth;
+
+  const progresso = new Progresso();
+  const barreiras = new Barreiras(altura, largura, 200, 400, () =>
+    progresso.atualizarPontos(++pontos)
+  );
+  const passaro = new Passaro(altura);
+
+  areaDoJogo.appendChild(progresso.elemento);
+  areaDoJogo.appendChild(passaro.elemento);
+  barreiras.pares.forEach((par) => areaDoJogo.appendChild(par.elemento));
+
+  this.start = () => {
+    const temporizador = setInterval(() => {
+      barreiras.animar();
+      passaro.animar();
+    }, 20);
+  };
+}
+
+new FlappyBird().start();
