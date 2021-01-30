76f827ce975096c7c6d91c89141710346049db92
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-30 13:37:48 -0300

    Flappy Bird #6

--- s14-integrando-html-js-css/js/flappy.js
+++ s14-integrando-html-js-css/js/flappy.js
@@ -121,6 +121,32 @@ function Progresso() {
   this.atualizarPontos(0);
 }
 
+function estaoSobrepostos(elementoA, elementoB) {
+  const a = elementoA.getBoundingClientRect();
+  const b = elementoB.getBoundingClientRect();
+
+  const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left;
+
+  const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top;
+
+  return horizontal && vertical;
+}
+
+function colidir(passaro, barreiras) {
+  let colidiu = false;
+  barreiras.pares.forEach((parDeBarreiras) => {
+    if (!colidiu) {
+      const superior = parDeBarreiras.superior.elemento;
+      const inferior = parDeBarreiras.inferior.elemento;
+
+      colidiu =
+        estaoSobrepostos(passaro.elemento, superior) ||
+        estaoSobrepostos(passaro.elemento, inferior);
+    }
+  });
+  return colidiu;
+}
+
 function FlappyBird() {
   let pontos = 0;
 
@@ -142,6 +168,10 @@ function FlappyBird() {
     const temporizador = setInterval(() => {
       barreiras.animar();
       passaro.animar();
+
+      if (colidir(passaro, barreiras)) {
+        clearInterval(temporizador);
+      }
     }, 20);
   };
 }
