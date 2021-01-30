e428e3cf9e9d9574880089864be2cce06676cead
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-29 22:01:14 -0300

    Flappy Bird #3

--- s14-integrando-html-js-css/js/flappy.js
+++ s14-integrando-html-js-css/js/flappy.js
@@ -43,5 +43,38 @@ function ParDeBarreiras(altura, abertura, x) {
   this.setX(x);
 }
 
-const b = new ParDeBarreiras(700, 200, 400);
-document.querySelector("[wm-flappy").appendChild(b.el);
+// const b = new ParDeBarreiras(700, 200, 400);
+// document.querySelector("[wm-flappy").appendChild(b.el);
+
+function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
+  this.pares = [
+    new ParDeBarreiras(altura, abertura, largura),
+    new ParDeBarreiras(altura, abertura, largura + espaco),
+    new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+    new ParDeBarreiras(altura, abertura, largura + espaco * 3),
+  ];
+
+  const deslocamento = 3;
+  this.animar = () => {
+    this.pares.forEach((par) => {
+      par.setX(par.getX() - deslocamento);
+
+      if (par.getX() < -par.getLargura()) {
+        par.setX(par.getX() + espaco * this.pares.length);
+        par.sortearAbertura();
+      }
+
+      const meio = largura / 2;
+      const cruzaOMeio = par.getX() + deslocamento >= meio && par.getX() < meio;
+
+      if (cruzaOMeio) notificarPonto();
+    });
+  };
+}
+
+const barreiras = new Barreiras(700, 1200, 200, 400);
+const areaDoJogo = document.querySelector("[wm-flappy]");
+barreiras.pares.forEach((par) => areaDoJogo.appendChild(par.el));
+setInterval(() => {
+  barreiras.animar();
+}, 20);
