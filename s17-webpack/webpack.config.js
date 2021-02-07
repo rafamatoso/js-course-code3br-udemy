e70c2d65c3340b6a975d9a7963f3a51f8b747e3e
Rafael Matoso <rafaelmatosog@gmail.com>
2021-02-06 22:16:43 -0300

    Webpack: adicionando web server

--- s17-webpack/webpack.config.js
+++ s17-webpack/webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
     filename: "principal.js",
     path: __dirname + "/public",
   },
+  devServer: {
+    contentBase: "./public",
+    port: 9000,
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "estilo.css",
