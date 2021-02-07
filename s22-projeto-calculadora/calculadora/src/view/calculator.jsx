fd4861b4a5adca244ab39573981ee11dc99850b2
Rafael Matoso <rafaelmatosog@gmail.com>
2021-02-07 20:48:07 -0300

    Implementando lógica #1

--- s22-projeto-calculadora/calculadora/src/view/calculator.jsx
+++ s22-projeto-calculadora/calculadora/src/view/calculator.jsx
@@ -4,7 +4,17 @@ import "./calculator.css";
 import Display from "../components/Display";
 import Button from "../components/Button";
 
+const initialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
 export default class Calculator extends Component {
+  state = { ...initialState };
+
   constructor(props) {
     super(props);
     this.clearMemory = this.clearMemory.bind(this);
@@ -13,7 +23,7 @@ export default class Calculator extends Component {
   }
 
   clearMemory() {
-    console.log("limpar");
+    this.setState({ ...initialState });
   }
 
   setOperation(operation) {
@@ -21,13 +31,30 @@ export default class Calculator extends Component {
   }
 
   addDigit(n) {
-    console.log(n);
+    if (n === "." && this.state.displayValue.includes(".")) {
+      return;
+    }
+
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.clearDisplay;
+
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (n !== ".") {
+      const index = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[index] = newValue;
+      this.setState({ values });
+    }
   }
 
   render() {
     return (
       <div className="calculator">
-        <Display value={100}></Display>
+        <Display value={this.state.displayValue}></Display>
         <Button label="AC" click={this.clearMemory} triple></Button>
         <Button label="/" click={this.setOperation} operation></Button>
         <Button label="7" click={this.addDigit}></Button>
