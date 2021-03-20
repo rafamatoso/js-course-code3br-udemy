f928ca9ca94eafbb759a6be7da0891abbfd47236
Rafael Matoso <rafaelmatosog@gmail.com>
2021-03-20 17:24:00 -0300

    Componente Logo

--- s23-projeto-crud/frontend/src/components/template/Logo/index.jsx
+++ s23-projeto-crud/frontend/src/components/template/Logo/index.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import logo from "../../../assets/img/logo.png";
 import "./styles.css";
 
-export default (props) => <aside className="logo">Logo</aside>;
+export default (props) => (
+  <aside className="logo">
+    <a href="/" className="logo">
+      <img src={logo} alt="logo" />
+    </a>
+  </aside>
+);
